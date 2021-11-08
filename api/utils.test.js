b13b7601a11ba1f9fa1873c5a49ef45246b3b516
Romer Maldonado <rocaley14@gmail.com>
2021-11-08 17:46:01 -0400

    improved: test naming

--- api/utils.test.js
+++ api/utils.test.js
@@ -1,33 +1,33 @@
 /* eslint-env mocha */
-const expect = require('chai').expect
-const utils = require('./utils')
-const { reverseString, isPalindrome } = utils
+const expect = require("chai").expect;
+const utils = require("./utils");
+const { reverseString, isPalindrome } = utils;
 
-describe('reverse string', function () {
-  it('reverse string empty', async function () {
-    const reversedText = reverseString('')
-    expect(reversedText).to.eql('')
-  })
+describe("Reverse string function", function () {
+  it("reverseString('') returns ''", async function () {
+    const reversedText = reverseString("");
+    expect(reversedText).to.eql("");
+  });
 
-  it('reverse string works', async function () {
-    const reversedText = reverseString('prueba')
-    expect(reversedText).to.eql('abeurp')
-  })
-})
+  it("reverseString('prueba') returns 'abeurp'", async function () {
+    const reversedText = reverseString("prueba");
+    expect(reversedText).to.eql("abeurp");
+  });
+});
 
-describe('palindrome', function () {
-  it('palindrome false', async function () {
-    const palindrome = isPalindrome('not a palindrome')
-    expect(palindrome).to.be.false
-  })
+describe("Palindrome function", function () {
+  it("palindrome: not a palindrome = false", async function () {
+    const palindrome = isPalindrome("not a palindrome");
+    expect(palindrome).to.be.false;
+  });
 
-  it('palindrome true', async function () {
-    const palindrome = isPalindrome('race car')
-    expect(palindrome).to.be.true
-  })
+  it("palindrome: race car = true", async function () {
+    const palindrome = isPalindrome("race car");
+    expect(palindrome).to.be.true;
+  });
 
-  it('palindrome true', async function () {
-    const palindrome = isPalindrome('My age is 0, 0 si ega ym.')
-    expect(palindrome).to.be.true
-  })
-})
+  it("palindrome: My age is 0, 0 si ega ym = true", async function () {
+    const palindrome = isPalindrome("My age is 0, 0 si ega ym");
+    expect(palindrome).to.be.true;
+  });
+});
